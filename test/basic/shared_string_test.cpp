6eee05d4d6d0bcb1df33bf951bee5c30f6e31740
Mark Zeren <mzeren@vmware.com>
2017-05-16 05:52:37 -0700

    test: Check ctrl_block inst count in shared_string tests

--- test/basic/shared_string_test.cpp
+++ test/basic/shared_string_test.cpp
@@ -9,16 +9,49 @@ using namespace rethink;
 using namespace rethink::detail;
 using namespace std;
 
-TEST_CASE("Empty shared string is empty", "[ref_string]") {
+TEST_CASE("Empty shared string is empty", "[shared_string]") {
   shared_string s;
   CHECK(string_size(s) == 0);
 }
 
-TEST_CASE("Shared strings init from ref_string", "[ref_string]") {
-  ref_string r = "foo";
-  shared_string s(r);
-  CHECK(string_size(s) == 3);
-  CHECK(s.size() == 3);
-  CHECK(strncmp(string_data(s), r.data(), s.size()) == 0);
-  CHECK(strncmp(s.data(), r.data(), s.size()) == 0);
+TEST_CASE("Shared string value ops", "[shared_string]") {
+  size_t start = ctrl_block::instance_count();
+
+  SECTION("Shared strings init from ref_string") {
+    ref_string r = "foo";
+    shared_string s(r);
+    CHECK(string_size(s) == 3);
+    CHECK(s.size() == 3);
+    CHECK(strncmp(string_data(s), r.data(), s.size()) == 0);
+    CHECK(strncmp(s.data(), r.data(), s.size()) == 0);
+  }
+
+  SECTION("Shared strings assigned from ref_string") {
+    shared_string s;
+    ref_string r = "foo";
+    s = r;
+    CHECK(string_size(s) == 3);
+    CHECK(s.size() == 3);
+    CHECK(strncmp(string_data(s), r.data(), s.size()) == 0);
+    CHECK(strncmp(s.data(), r.data(), s.size()) == 0);
+  }
+
+  SECTION("Shared string move copied") {
+    shared_string from = ref_string("foo");
+    shared_string s(move(from));
+    CHECK(from.size() == 0);
+    CHECK(s.size() == 3);
+    CHECK(strncmp(s.data(), "foo", s.size()) == 0);
+  }
+
+  SECTION("Shared string move assigned") {
+    shared_string from = ref_string("foo");
+    shared_string s;
+    s = move(from);
+    CHECK(from.size() == 0);
+    CHECK(s.size() == 3);
+    CHECK(strncmp(s.data(), "foo", s.size()) == 0);
+  }
+
+  CHECK(ctrl_block::instance_count() == start);
 }
